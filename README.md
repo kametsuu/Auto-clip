75b9f75e4d188f19a49ac72616b82c1dadd331aa
kametsuu <116424124+kametsuu@users.noreply.github.com>
2022-10-23 11:48:23 +0200

    Update 0.2

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-"# Clipper" 
+# Autoclip - version 0.2
+## _Generates clip mask by subtracting images and turns it to vector mask_
+
+_Sadly for now it has it's flaws, however as time passes I ought to improve as much as I can, however it's nearly impossible to make it work perfectly._
+
+For now it struggle with:
+- Sharp edges
+- Moving scenes
+
+## How to configure
+
+- Open autoclip.lua file and at the very top replace in the `exec` variable path of the . exe file.
+
+## How to use
+Go to frame, where there is no object and then open GUI and set minuend (by clicking the relevant button for type of frame it'll automatically set current video time). Then skip to the same frame but now with appeared object and set range by adequately seting subtrahend "from" and "to" frames (If you want just one frame, then set time on both frames the same). When everything will be done, click "apply" button. If your computer is slow or/and you choosen large frame range, it may take some time.
+
+## Params
+While frame times are intuitive, other options may not be, so I'll explain them.
+- Erosion level - If you had a problem that clip outline is too inward or outward, then by choosing different level of it may fix that problem.
+- Minimum area - If you want to get rid of smaller objects, you can adjust that all contours whose area is smaller than this value will be removed.
+- Cliptype - If you want the inside of the mask to be visible instead of the outside, just change it from \iclip to \clip.
+
+## Changelog
+0.2 - Improved mask detection, added GUI, added functionality to use fragments rather than just two choosen frames, added settings (Erosion level, Minimum area and Clip type)
+0.0.1 - Initial commit
--- autoclip.lua
+++ autoclip.lua
@@ -1,54 +1,118 @@
--- basic script info
+----- Config (example) -----
+exec = "C:\\Users\\Kametsu\\Documents\\GitHub\\autoclip.exe"
+-- To set everything up so it'll work, for each slash you need to add one additional slash to escape them.
+
+
+----- Basic script info -----
 script_name="Auto Clip"
-script_description="Makes automaticly clipmask"
+script_description="Subtracts subtrahend frame from minuend frame and transforms that into vector"
 script_author="kametsu"
-script_version="0.0.1"
-
--- path to exe file | example:
-exe = "C:\\Users\\Kametsu\\Desktop\\clipmaskfunc.exe"
+script_version="0.2"
 
 
-function set_minuend_frame()
-    -- load aegienv properties and write timeframe to file
-    properties = aegisub.project_properties()
-    frame = properties.video_position
-    file = io.open("minuendframe.cfg", "w")
-
-    file:write(frame)
-    file:close()
+----- Repetetive functions -----
+function split(s, delimiter)
+    result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
 end
 
+function add_line(s, l, txt, p, s_, e_)
+    s_ = aegisub.ms_from_frame(s_)
+    e_ = aegisub.ms_from_frame(e_+1)
 
-function create_mask(sub, sel)
-    -- Opening minuend timeframe
-    file = io.open("minuendframe.cfg", "r")
-    minuend_frame = file:read()
+    l.text = txt .. l.text
+    l.start_time = s_
+    l.end_time = e_
+
+    s.insert(p, l)
+end
+
+function write_to_memory(a, b, c)
+    file = io.open("autoclip.cfg", "w")
+    file:write(a .. '\n' .. b .. '\n' .. c)
     file:close()
 
-    -- get timeframe of subtrahend frame and path of video
+end
+
+function read_memory()
+    file = io.open("autoclip.cfg", "a+")
+    config = split(file:read("*all"), '\n')
+    if #config == 1 then
+        write_to_memory("0", "0", "0")
+        return {0, 0, 0}
+    else
+        return config
+    end
+end
+
+----- Functions -----
+
+function apply(sub, sel, frames, erosion_, min_area_, cliptype_)
     properties = aegisub.project_properties()
-    subtrahend_frame = properties.video_position
-    video_path = properties.video_file
+    video = properties.video_file
 
-    -- create command
-    cmd = "\"\"" .. exe .. "\" \"" .. video_path .. "\" \"" ..  minuend_frame .. "," .. subtrahend_frame .. "\"\""
+    frames_ = frames[1] .. ',' .. frames[2] .. ',' .. frames[3]
 
-    -- opens io subprocess and assert command
-    handle = assert(io.popen(cmd, 'r'))
+    args = {exec, video, frames_, erosion_, min_area_, cliptype_}
+
+
+    command = "\"\"" .. table.concat(args, "\" \"") .. "\"\""
+
+    handle = assert(io.popen(command, 'r'))
     output = assert(handle:read('*a'))
     handle:close()
 
-    -- get pointer to first sub and swap line
-    pointer = sel[1]
-    line = sub[pointer]
-    line.text = output:sub(1, -2) .. line.text
-    sub[pointer] = line
+    commands = split(split(output, 'Done\n')[2], ';')
 
-    return sel
+    pointer = sel[1]
+
+    for i, line in pairs(commands) do
+        line_split = split(line, ':')
+
+        start = tonumber(line_split[1])
+        text = line_split[2]
+        
+        add_line(sub, sub[pointer], text, pointer+i, start, start)
+    end
+end
+
+function gui(sub, sel)
+    cfg = read_memory()
+
+    minuend_frame_ts = tonumber(cfg[1])
+    subtrahend_from_frame_ts = tonumber(cfg[2])
+    subtrahend_to_frame_ts = tonumber(cfg[3])
+
+    properties = aegisub.project_properties()
+    time = properties.video_position
+
+    dialog_config=
+    {
+        {x=0,y=0,width=1,height=1,class="label",label="Minuend frame: "},
+        {x=1,y=0,width=1,height=1,class="intedit", name="minuend",value=minuend_frame_ts},
+        {x=0,y=1,width=1,height=1,class="label",label="Subtrahend frames from: "},
+        {x=1,y=1,width=1,height=1,class="intedit", name="subtrahends_from",value=subtrahend_from_frame_ts},
+        {x=0,y=2,width=1,height=1,class="label",label="Subtrahend frames to: "},
+        {x=1,y=2,width=1,height=1,class="intedit", name="subtrahends_to",value=subtrahend_to_frame_ts},
+        {x=2,y=0,width=1,height=1,class="label",label="Erosion level: "},
+        {x=3,y=0,width=1,height=1,class="intedit", name="erosion",value=4},
+        {x=2,y=1,width=1,height=1,class="label",label="Minimum area: "},
+        {x=3,y=1,width=1,height=1,class="intedit",name="min_area",value=1000},
+        {x=2,y=2,width=1,height=1,class="label",label="Clip: "},
+        {x=3,y=2,width=1,height=1,class="dropdown",items={"\\iclip", "\\clip"},name="cliptype",value="\\iclip"}
+    } 	
+    buttons={"Set minuend","Set subtrahend from", "Set subtrahend to", "Save from input", "Apply", "Cancel"}
+    pressed,res=aegisub.dialog.display(dialog_config,buttons)
+    if pressed=="Cancel" then aegisub.cancel() end
+    if pressed=="Set minuend" then write_to_memory(time, res.subtrahends_from, res.subtrahends_to) end
+    if pressed=="Set subtrahend from" then write_to_memory(res.minuend, time, res.subtrahends_to) end
+    if pressed=="Set subtrahend to" then write_to_memory(res.minuend, res.subtrahends_from, time) end
+    if pressed=="Save from input" then write_to_memory(res.minuend, res.subtrahends_from, res.subtrahends_to) end
+    if pressed=="Apply" then apply(sub, sel, {res.minuend, res.subtrahends_from, res.subtrahends_to}, tostring(res.erosion), tostring(res.min_area), res.cliptype) end
 end
 
 
--- Sets aegi macros
-aegisub.register_macro("Autoclip - Base frame", "Sets minuend frame", set_minuend_frame)
-aegisub.register_macro("Autoclip - Subtrahend frame & apply", "Will subtract subtrahend frame from minuend frame making clip", create_mask)
-
+aegisub.register_macro("Autoclip", "Subtracts subtrahend frame from minuend frame and transforms that into vector", gui)
